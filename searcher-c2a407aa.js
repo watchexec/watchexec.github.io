da07aba72c68cfb3eb7dfadcea2ee46696576f36
passcod <felix@passcod.name>
2026-03-30 06:36:52 +0000

    passcod published a site update

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-258a270f.js',
+                path_to_root + 'searchindex-12f3009b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
