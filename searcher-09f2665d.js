4cd00920066fbc4096ade44f39f96f0dfcf55b12
passcod <felix@passcod.name>
2026-08-23 15:47:00 +0000

    passcod published a site update

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1547f551.js',
+                path_to_root + 'searchindex-3ed45c42.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
